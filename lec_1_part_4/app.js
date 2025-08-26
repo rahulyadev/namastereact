eb765b2a0b3480954b75f6e5f42a5e8ab0db6775
Rahul Yadav <parryrpy@gmail.com>
2025-08-26 11:24:03 +0000

    Lecture 1: Part 4

---
+++ lec_1_part_4/app.js
@@ -0,0 +1,22 @@
+const parent = React.createElement('div', {id: 'parent'}, 
+    React.createElement(
+        'div',
+        {id: 'child'},
+        [React.createElement(
+            'h1',
+            {},
+            'I am h1 tag'
+        ),
+    React.createElement(
+            'h2',
+            {},
+            'I am h2 tag'
+        )]
+    )
+)
+
+// ReactElement is object & render makes it into a real DOM element
+
+// const heading = React.createElement('h1', {id: 'heading', xyz: 'abc'}, 'Hello from React!');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
