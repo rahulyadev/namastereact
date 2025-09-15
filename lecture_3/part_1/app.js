02e54df7302a3a9c1dbad6e586f7d0b9375c3e75
Rahul Yadav <66272748+rahulyadev@users.noreply.github.com>
2025-09-15 13:39:27 +0530

    Lecture 3: Till part 4

--- lecture_3/part_1/app.js
+++ lecture_3/part_1/app.js
@@ -1,1 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
+const Title = (
+    <h1 className="title" tabIndex="5">
+        This is the Title
+    </h1>
+)
+
+// const HeadingComponent = () => {
+//     return <h1>React functional component</h1>;
+// }
+
+// const HeadingComponent = () => <h1>React functional component</h1>;
+
+const HeadingComponent = () => (
+    <div id="container">
+        <Title />
+        <h1>
+            React functional component
+        </h1>
+    </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<HeadingComponent />);
